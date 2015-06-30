23333a55631f1cac75863893e89ac4a17e98e235
Bryan <brymac@gmail.com>
2015-06-30 15:23:14 -0400

    SASS updates

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Product.create(name: "Mouse", description: "Use your laptop with this handy mouse", price_in_cents: 4000)
+Product.create(name: "Trackpad", description: "Great accuracy!", price_in_cents: 5000)
+Product.create(name: "Keyboard", description: "Wireless freedom with Bluetooth.  Live wirelessly!", price_in_cents: 5000)
+Product.create(name: "Monitor", description: "See clearly with this Retina display", price_in_cents: 70000)
+
+
+
