dbaff2efafb9da2054579c018f8a6e864f275107
Bryan <brymac@gmail.com>
2015-07-16 14:56:56 -0400

    Adding infinite scroll

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -1,9 +1,15 @@
-$(document).on('ready page:load', function() {
+ $(document).on('ready page:load', function() {
   $('#search-form').submit(function(event) {
     event.preventDefault();
     var searchValue = $('#search').val();
+
     $.getScript('/products?search=' + searchValue);
   });
 
-  
-});
+   $(window).scroll(function() {
+     if ($(window).scrollTop() > $(document).height() - $(window).height() - 50) {
+       console.log($('.pagination span.next').children().attr('href'));
+       $.getScript($('.pagination span.next').children().attr('href'));
+     }
+   });
+ });
